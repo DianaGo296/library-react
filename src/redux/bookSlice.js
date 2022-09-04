6987005e3424736b9b0664ef80349eecfff26a80
Diana Gorodinsky <Diana.Gorodinsky@ewave.co.il>
2022-09-04 17:53:06 +0300

    redux

--- src/redux/bookSlice.js
+++ src/redux/bookSlice.js
@@ -1,10 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { collection, getDocs, updateDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, doc, setDoc } from 'firebase/firestore';
 import db from '../firebase/firebase';
 
 const bookCollection = collection(db, 'books');
 
-export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+let getAllBooks = async () => {
     try {
         const snapshot = await getDocs(bookCollection);
         return snapshot.docs.map((doc) => {
@@ -16,19 +16,42 @@ export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
     catch (e) {
         console.log(e)
     }
+}
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+    getAllBooks();
 });
 
 
 
 export const updateBooks = createAsyncThunk('books/updateBooks', async (data) => {
     // console.log(data)
-
-    const bookRef = doc(db, "books", data.id);
     try {
-        return await updateDoc(bookRef, {
+        const bookRef = doc(db, "books", data.id);
+
+        const newData = {
             availble: data.availble,
             userName: data.userName
-        }, { merge: true })
+        }
+
+        await setDoc(bookRef, newData, { merge: true })
+
+
+        // try {
+        //     const snapshot = await getDocs(bookCollection);
+        //     const newResults = snapshot.docs.map((doc) => {
+        //         const data = doc.data();
+        //         const id = doc.id;
+        //         return { id, ...data };
+        //     });
+        //     console.log(newResults)
+        //     return newResults
+        // }
+        // catch (e) {
+        //     console.log(e)
+        // }
+
+
     }
     catch (e) {
         console.log(e)
@@ -64,16 +87,15 @@ export const bookSlice = createSlice({
 
 
 
-        builder.addCase(updateBooks.pending, (state) => {
-            state.status = 'loading'
-        });
+        // builder.addCase(updateBooks.pending, (state) => {
+        //     state.status = 'loading'
+        // });
 
         builder.addCase(updateBooks.fulfilled, (state, action) => {
-           console.log(state.books = action.payload)
+
         });
 
         builder.addCase(updateBooks.rejected, (state, action) => {
-            state.status = 'failed'
             state.error = action.error.message
         });
     }
